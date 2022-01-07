289883dced568d62874d51c00a8cee2be5bffeed
hk <2971913255@qq.com>
2022-01-07 19:44:59 +0800

    ss

--- src/directives/mouse-wheel.ts
+++ src/directives/mouse-wheel.ts
@@ -2,59 +2,41 @@ import normalizeWheel from 'normalize-wheel-es'
 import { isFirefox } from '../utils/util'
 import type { ObjectDirective, DirectiveBinding } from 'vue'
 
-interface Fun1 {
+const perventMap = new Map();
+interface AddWheel {
   (element: HTMLElement, callback: any, modifiers: Record<string, any>): void;
 }
-interface Fun2 {
-  (p: boolean): void;
-}
-
-const addWheelListenerCLosure = () => {
-  let prevent = false;
-  const setPervent: Fun2 = (p) => {
-    prevent = p;
-  }
-  const addWheelListener: Fun1 = function (element, callback, modifiers = {}) {
-    if (element && element.addEventListener) {
-      const fn = function (this: any, event: any) {
-        if (prevent) {
-          event.preventDefault && event.preventDefault();
-        }
-        if (modifiers.stop) {
-          event.stopPropagation && event.stopPropagation();
-        }
-        const normalized = normalizeWheel(event);
-        callback && callback.apply(this, [event, normalized]);
+const addWheelListener: AddWheel = function (element, callback, modifiers = {}) {
+  if (element && element.addEventListener) {
+    const fn = function (this: any, event: any) {
+      const prevent = perventMap.get(element);
+      if (prevent) {
+        event.preventDefault && event.preventDefault();
       }
-      if (isFirefox.value) {
-        element.addEventListener('DOMMouseScroll', fn);
-      } else {
-        element.addEventListener('mousewheel', fn);
+      if (modifiers.stop) {
+        event.stopPropagation && event.stopPropagation();
       }
+      const normalized = normalizeWheel(event);
+      callback && callback.apply(this, [event, normalized]);
+    }
+    if (isFirefox.value) {
+      element.addEventListener('DOMMouseScroll', fn);
+    } else {
+      element.addEventListener('mousewheel', fn);
     }
   }
-  let res: [Fun1, Fun2] = [addWheelListener, setPervent];
-  return res;
 }
 
 const Mousewheel: ObjectDirective = {
   beforeMount(el: HTMLElement, binding: DirectiveBinding) {
-    const [addWheelListener, setPervent] = addWheelListenerCLosure();
-    if (binding.arg) {
-      setPervent(true);
-    } else {
-      setPervent(false);
-    }
-    el.__setPervent__ = setPervent;
+    perventMap.set(el, Boolean(binding.arg));
     addWheelListener(el, binding.value, binding.modifiers);
   },
   beforeUpdate(el: HTMLElement, binding: DirectiveBinding) {
-    const setPervent = el.__setPervent__;
-    if (binding.arg) {
-      setPervent(true);
-    } else {
-      setPervent(false);
-    }
+    perventMap.set(el, Boolean(binding.arg))
+  },
+  beforeUnmount(el){
+    perventMap.delete(el);
   }
 }
 
